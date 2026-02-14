c7571ccb5a757cb9667436420b021d2786befad8
milkshake308 <rnsaa@proton.me>
2026-02-14 18:15:33 +0100

    fix: disable services

--- lib/service.dart
+++ lib/service.dart
@@ -1,46 +1,57 @@
-import 'dart:async';
+// import 'dart:async';
 
-import 'package:flasher/models/io_progress.dart';
-import 'package:flasher/models/target_disk.dart';
-import 'package:flasher/models/user_file.dart';
+// import 'package:flasher/models/io_progress.dart';
+// import 'package:flasher/models/target_disk.dart';
+// import 'package:flasher/models/user_file.dart';
+// import 'package:flasher/repository/io_backend_repository.dart';
+// import 'package:flasher/repository/user_file_repository.dart';
 
-class FlasherService {
-  Future<Stream<IoProgress>> flashWithProgress(
-    UserFile sourceFile,
-    TargetDisk destinationDisk,
-  ) async {
-    final ioProgressController = StreamController<IoProgress>();
-    final sourceStream = sourceFile.readStream().handleError(
-      ioProgressController.addError,
-    );
-    final destinationWritter = await destinationDisk.ioWritterFromPlatform();
+// class Services {
+//   final IOBackendRepository _ioBackendRepository;
+//   final UserFileRepository _userFileRepository;
 
-    unawaited(() async {
-      final total = sourceFile.size;
-      var current = 0;
+//   Services({
+//     IOBackendRepository? destinationWritterRepository,
+//     UserFileRepository? userFileRepository,
+//   })
 
-      try {
-        await for (final chunk in sourceStream) {
-          destinationWritter.add(chunk);
-          await destinationWritter.flush();
-          current += chunk.length;
-          ioProgressController.add(IoProgress(current: current, total: total));
-        }
-      } catch (e) {
-        ioProgressController.addError(e);
-      }
+//   Future<Stream<IoProgress>> flashWithProgress(
+//     UserFile sourceFile,
+//     TargetDisk destinationDisk,
+//   ) async {
+//     final ioProgressController = StreamController<IoProgress>();
+//     final sourceStream = _userFileRepository.readStream(sourceFile).handleError(
+//       ioProgressController.addError,
+//     );
+//     final destinationWritter = await _ioBackendRepository
+//         .ioWritterFromPlatform(destinationDisk);
 
-      // we want to always call finish no matter what,
-      // and pass error to consumer of stream
-      try {
-        await destinationWritter.finish();
-      } catch (e) {
-        ioProgressController.addError(e);
-      }
+//     unawaited(() async {
+//       final total = sourceFile.size;
+//       var current = 0;
 
-      await ioProgressController.close();
-    }());
+//       try {
+//         await for (final chunk in sourceStream) {
+//           destinationWritter.add(chunk);
+//           await destinationWritter.flush();
+//           current += chunk.length;
+//           ioProgressController.add(IoProgress(current: current, total: total));
+//         }
+//       } catch (e) {
+//         ioProgressController.addError(e);
+//       }
 
-    return ioProgressController.stream;
-  }
-}
+//       // we want to always call finish no matter what,
+//       // and pass error to consumer of stream
+//       try {
+//         await destinationWritter.finish();
+//       } catch (e) {
+//         ioProgressController.addError(e);
+//       }
+
+//       await ioProgressController.close();
+//     }());
+
+//     return ioProgressController.stream;
+//   }
+// }
